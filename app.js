b528956f81a708705a86e455b894818d0974b78a
M-X-3112 <mahmudxudayarli18@gmail.com>
2024-10-18 23:24:54 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const day = document.getElementById("day")
+const weekday = document.getElementById("weekday")
+const month = document.getElementById("month")
+const year = document.getElementById("year")
+const monthsArr = ['January', 'February', 'March', 'April', 'May', 'June ', 'July', 'August', 'September', 'October', 'November', 'December' ]
+const weekArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday ', 'Saturday']
+const currentTime = new Date();
+console.log(currentTime);
+
+
+day.innerHTML = currentTime.getDate();
+weekday.innerHTML = weekArr[currentTime.getDay()];
+month.innerHTML = monthsArr[currentTime.getMonth()];
+year.innerHTML = currentTime.getFullYear();
